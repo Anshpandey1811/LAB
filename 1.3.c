189cd321555cdfef103bc3084b032d7ee03df0d8
Ansh Pandey <pandeyansh486@gmail.com>
2024-12-18 14:39:59 +0530

    Update 1.3.c

--- 1.3.c
+++ 1.3.c
@@ -1,7 +1,8 @@
+//define the lenth and width//
 #include<stdio.h>
 int main(){
 	int a,b;
-	printf("enter lenth and breadth of rectangle: ");
+	printf("enter lenth and width of rectangle: ");
 	scanf("%d %d",&a,&b);
 	printf("area of rectangle: %d",a*b);
 	return 0;
